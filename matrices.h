3c3c82644c85f46f9ec2c144d382df8984eb8792
0dxplt <154014757+0dxplt@users.noreply.github.com>
2024-05-19 13:24:58 +0200

    .

--- matrices.h
+++ matrices.h
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NULLVALUE -1
-
 typedef struct matrix *MatrixPtr;
 
 MatrixPtr initMatrix(int rows, int columns);
-MatrixPtr Sum(MatrixPtr a, MatrixPtr b);
-MatrixPtr Diff(MatrixPtr a, MatrixPtr b);
-MatrixPtr Mul(MatrixPtr a, MatrixPtr b);
-int Det(MatrixPtr a);
+MatrixPtr Sum(MatrixPtr matrix, MatrixPtr b);
+MatrixPtr Diff(MatrixPtr matrix, MatrixPtr b);
+MatrixPtr Mul(MatrixPtr matrix, MatrixPtr b);
+MatrixPtr Square(MatrixPtr matrix);
+MatrixPtr Reduce(MatrixPtr matrix);
+int Rank(MatrixPtr matrix);
+int Det(MatrixPtr matrix);
+size_t isTriangularI(MatrixPtr matrix);
+size_t isTriangularS(MatrixPtr matrix);
+size_t isTriangular(MatrixPtr matrix);
 void printMatrix(MatrixPtr matrix);
